d42bf70fb23aa084c77d2d6d3c35158b483b07fb
acn-sbuad <stephanie.buadu@avanade.com>
2024-03-15 12:10:49 +0100

    removed sampling settings

--- src/Altinn.Notifications.Sms/Configuration/CustomTelemetryInitializer.cs
+++ src/Altinn.Notifications.Sms/Configuration/CustomTelemetryInitializer.cs
@@ -16,24 +16,7 @@ public class CustomTelemetryInitializer : ITelemetryInitializer
     {
         if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
         {
-            telemetry.Context.Cloud.RoleName = "platform-notifications-sms";
+            telemetry.Context.Cloud.RoleName = "local-platform-notifications-sms";
         }
-
-        // Disable sampling for exceptions, requests, dependencies and cleanup
-        if (telemetry is RequestTelemetry requestTelemetry)
-        {
-            ((ISupportSampling)telemetry).SamplingPercentage = 100;
-            requestTelemetry.ProactiveSamplingDecision = SamplingDecision.SampledIn;
-        }
-        else if (telemetry is DependencyTelemetry dependencyTelemetry)
-        {
-            ((ISupportSampling)telemetry).SamplingPercentage = 100;
-            dependencyTelemetry.ProactiveSamplingDecision = SamplingDecision.SampledIn;
-        }
-        else if (telemetry is ExceptionTelemetry exceptionTelemetry)
-        {
-            ((ISupportSampling)telemetry).SamplingPercentage = 100;
-            exceptionTelemetry.ProactiveSamplingDecision = SamplingDecision.SampledIn;
-        }       
     }
 }
