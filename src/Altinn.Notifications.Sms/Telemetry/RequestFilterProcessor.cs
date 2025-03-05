e7f9bedada2e9b5e2025ce88f252fddc4ed934eb
Sigrid Bratsberg <sigrid.bratsberg@digdir.no>
2025-03-05 08:35:04 +0100

    Fix inverted if

--- src/Altinn.Notifications.Sms/Telemetry/RequestFilterProcessor.cs
+++ src/Altinn.Notifications.Sms/Telemetry/RequestFilterProcessor.cs
@@ -56,7 +56,7 @@ namespace Altinn.Notifications.Sms.Telemetry
 
         private static bool ExcludeRequest(string? localpath)
         {
-            if (!string.IsNullOrEmpty(localpath))
+            if (string.IsNullOrEmpty(localpath))
             {
                 return false;
             }
