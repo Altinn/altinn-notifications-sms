a86b996a002597aca1d805f3f0b6c39bfbc42ed0
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-06-27 08:20:12 +0200

    Implement two new data transfer models used to send one-time passwords via SMS

---
+++ src/Altinn.Notifications.Sms/Models/OneTimePassword/OneTimePasswordRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Sms.Models.OneTimePassword;
+
+/// <summary>
+/// Represents a request model for sending a one-time password via SMS.
+/// </summary>
+public record OneTimePasswordRequest
+{
+    /// <summary>
+    /// The message that contains the one-time password.
+    /// </summary>
+    [Required]
+    [JsonPropertyName("message")]
+    public required string Message { get; init; }
+
+    /// <summary>
+    /// The unique identifier that represents this SMS notification order.
+    /// </summary>
+    [Required]
+    [JsonPropertyName("notificationId")]
+    public required Guid NotificationId { get; init; }
+
+    /// <summary>
+    /// The phone number to which the message will be sent.
+    /// </summary>
+    [Phone]
+    [Required]
+    [JsonPropertyName("recipient")]
+    public required string Recipient { get; init; }
+
+    /// <summary>
+    /// The sender of the SMS.
+    /// This is typically the name, phone number or identifier of the service sending the SMS.
+    /// </summary>
+    [Required]
+    [JsonPropertyName("sender")]
+    public required string Sender { get; init; }
+}
---
+++ src/Altinn.Notifications.Sms/Models/OneTimePassword/OneTimePasswordResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Altinn.Notifications.Sms.Models.OneTimePassword;
+
+/// <summary>
+/// Represents a response model for sending a one-time password via SMS.
+/// </summary>
+public record OneTimePasswordResponse
+{
+    /// <summary>
+    /// Indicates whether the SMS was accepted by the service provider.
+    /// </summary>
+    /// <value>
+    /// <c>true</c> if the SMS was accepted by the service provider; otherwise, <c>false</c>.
+    /// </value>
+    [JsonPropertyName("isAccepted")]
+    public required bool IsAccepted { get; init; }
+
+    /// <summary>
+    /// The unique identifier that represents this SMS notification order.
+    /// </summary>
+    [JsonPropertyName("notificationId")]
+    public required Guid NotificationId { get; init; }
+}
