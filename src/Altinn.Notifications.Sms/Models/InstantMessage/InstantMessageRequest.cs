3268e25b68ea88af8458f335e90ed941c008ec63
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-02 13:00:09 +0200

    Improve the code documentation

--- src/Altinn.Notifications.Sms/Models/InstantMessage/InstantMessageRequest.cs
+++ src/Altinn.Notifications.Sms/Models/InstantMessage/InstantMessageRequest.cs
@@ -9,38 +9,38 @@ namespace Altinn.Notifications.Sms.Models.InstantMessage;
 public record InstantMessageRequest
 {
     /// <summary>
-    /// Gets the message content to be delivered to the recipient.
+    /// The message content to be delivered.
     /// </summary>
     [Required]
     [JsonPropertyName("message")]
     public required string Message { get; init; }
 
     /// <summary>
-    /// Gets the unique identifier of the notification order, of which this message is a part.
+    /// The unique identifier of the notification order, of which this message is a part.
     /// </summary>
     [Required]
     [JsonPropertyName("notificationId")]
     public required Guid NotificationId { get; init; }
 
     /// <summary>
-    /// Gets the recipient's phone number where the message will be sent.
+    /// The recipient's phone number where the message will be sent.
     /// </summary>
     [Required]
     [JsonPropertyName("recipient")]
     public required string Recipient { get; init; }
 
     /// <summary>
-    /// Gets the sender of the SMS, typically the service name, phone number, or identifier.
+    /// The sender of this message, typically the service name, phone number, or identifier.
     /// </summary>
     [Required]
     [JsonPropertyName("sender")]
     public required string Sender { get; init; }
 
     /// <summary>
-    /// Gets the time to live in seconds after which the message is expired.
+    /// The time to live in seconds after which the message is expired.
     /// </summary>
     [Required]
     [JsonPropertyName("timeToLive")]
-    [Range(1, 172800, ErrorMessage = "TimeToLive must be between 1 second and 48 hours (172800 seconds)")]
+    [Range(1, 172800, ErrorMessage = "TimeToLive must be between 1 second and 48 hours")]
     public required int TimeToLive { get; init; }
 }
