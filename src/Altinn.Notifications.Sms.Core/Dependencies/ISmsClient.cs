4ea2d75b1a6e05df0a5b9c9e4753f51577c808f8
Ahmed-Ghanam <ahmed.m.ghanam@outlook.com>
2025-07-02 11:32:56 +0200

    Add support for setting a custom TTL value in the gateway client

--- src/Altinn.Notifications.Sms.Core/Dependencies/ISmsClient.cs
+++ src/Altinn.Notifications.Sms.Core/Dependencies/ISmsClient.cs
@@ -1,18 +1,44 @@
 ﻿using Altinn.Notifications.Sms.Core.Sending;
 using Altinn.Notifications.Sms.Core.Shared;
 
-namespace Altinn.Notifications.Sms.Core.Dependencies
+namespace Altinn.Notifications.Sms.Core.Dependencies;
+
+/// <summary>
+/// Represents a contract for an SMS client that can send text messages using an external SMS service provider.
+/// </summary>
+/// <remarks>
+/// Implementations of this interface are responsible for interacting with specific SMS gateway providers,
+/// abstracting the details of transport mechanisms and protocols from the core application.
+/// </remarks>
+public interface ISmsClient
 {
     /// <summary>
-    /// This interface describes the public interface of a client able to send sms messages through an sms service.
-    /// </summary>  
-    public interface ISmsClient
-    {
-        /// <summary>
-        /// Method for requesting the sending on an sms message.
-        /// </summary>
-        /// <param name="sms">The sms to be sent</param>
-        /// <returns>An id for tracing the sucess of the task or an <see cref="SmsClientErrorResponse"/> it the task fails</returns>
-        public Task<Result<string, SmsClientErrorResponse>> SendAsync(Sending.Sms sms);
-    }
+    /// Sends an SMS message to a specified recipient using the default time-to-live.
+    /// </summary>
+    /// <param name="sms">An instance of <see cref="Sending.Sms"/> containing the recipient, sender, and message content.</param>
+    /// <returns>
+    /// A <see cref="Result{T, TError}"/> that represents the outcome of the send operation:
+    /// <list type="bullet">
+    ///   <item>On success: a unique string identifier for tracking the message.</item>
+    ///   <item>On failure: a <see cref="SmsClientErrorResponse"/> providing details about the error.</item>
+    /// </list>
+    /// </returns>
+    Task<Result<string, SmsClientErrorResponse>> SendAsync(Sending.Sms sms);
+
+    /// <summary>
+    /// Sends an SMS message to a specified recipient, allowing for a configurable time-to-live (TTL).
+    /// </summary>
+    /// <param name="sms">An instance of <see cref="Sending.Sms"/> containing the recipient, sender, and message content.</param>
+    /// <param name="timeToLiveInSeconds">
+    /// The time-to-live in seconds, indicating how long the message is valid. 
+    /// If the message cannot be delivered within this period, the delivery will be abandoned.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Result{T, TError}"/> that represents the outcome of the send operation:
+    /// <list type="bullet">
+    ///   <item>On success: a unique string identifier for tracking the message.</item>
+    ///   <item>On failure: a <see cref="SmsClientErrorResponse"/> providing details about the error.</item>
+    /// </list>
+    /// </returns>
+    Task<Result<string, SmsClientErrorResponse>> SendAsync(Sending.Sms sms, int timeToLiveInSeconds);
 }
